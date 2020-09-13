250606c0fa5b43fb32f72f4142f317ee6b6e7271
Your Name <you@example.com>
2020-09-14 00:35:19 +0800

    Added article generator

---
+++ app/business/article_generator.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import load_model
+from numpy.random import randn
+from PIL import Image
+import numpy as np
+
+class articleGenerator():
+    def __init__(self):
+        self.model = load_model("app/business/generator_RGB_asy_149.h5")
+
+    def generate_latent_points(self, latent_dim: int, n_samples: int):
+        # generate points in the latent space
+        x_input = randn(latent_dim * n_samples)
+        # reshape into a batch of inputs for the network
+        x_input = x_input.reshape(n_samples, latent_dim)
+        return x_input
+
+    def generateImages(self, nbrToGen: int=8) -> []:
+        latent_points = self.generate_latent_points(200, nbrToGen)
+        # generate images
+        images = []
+        X = self.model.predict(latent_points)
+        for i in range(X.shape[0]):
+            img_array = (X[i] * 127.5) + 127.5
+            img = Image.fromarray(img_array.astype(np.uint8), 'RGB')
+            images.append(img)
+            img.save('app/static/img/generated/' + str(i) + '.jpg')
+
+        return images
